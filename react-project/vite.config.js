f85fd3b1f171b1f0140afca91be71d11dcdeaaa2
FGioia94 <info@francescogioia.it>
2025-09-09 21:04:52 +0200

    test deploy

--- react-project/vite.config.js
+++ react-project/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 
 export default defineConfig({
-  base: '/deadline-manager-UI/', 
+  base: '/deadline-manager-FE/', 
   plugins: [react()],
 });
